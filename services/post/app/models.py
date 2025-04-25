650680279de2b210814ae149077a193a5fbd14a2
Yana Ananenko <yana.ann798@gmail.com>
2025-04-25 20:40:14 +0000

    task 4

--- services/post/app/models.py
+++ services/post/app/models.py
@@ -12,3 +12,22 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
     is_deleted = db.Column(db.Boolean, default=False)
 
+
+
+class Comment(db.Model):
+    tablename = 'comments'
+
+    comment_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    post_id = db.Column(db.String(36), db.ForeignKey('posts.post_id'), nullable=False)
+    user_id = db.Column(db.String(36), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    parent_comment_id = db.Column(db.String(36), nullable=True)  # если нужен вложенный ответ
+
+class Like(db.Model):
+    tablename = 'likes'
+
+    like_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    post_id = db.Column(db.String(36), db.ForeignKey('posts.post_id'), nullable=False)
+    user_id = db.Column(db.String(36), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
--- services/user/app/schemas.py
+++ services/user/app/schemas.py
@@ -1,9 +1,18 @@
 from flask_marshmallow import Marshmallow
+from .models import User
+
 ma = Marshmallow()
 
-class UserSchema(ma.Schema):
+class UserSchema(ma.SQLAlchemySchema):
     class Meta:
-        fields = ('user_id', 'username', 'email', 'created_at', 'updated_at')
+        model = User
+        load_instance = True
+
+    user_id = ma.auto_field()
+    username = ma.auto_field()
+    email = ma.auto_field()
+    created_at = ma.auto_field()
+    updated_at = ma.auto_field()
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
